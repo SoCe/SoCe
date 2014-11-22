bf84cf0814d79b696131a66c4cb8e5810bdfe70c
JuKu <kuenzel.justin@t-online.de>
2014-11-22 15:11:41 +0100

    change comparable interface in IModuleTask.

--- Lib/src/main/java/lib/task/IModuleTask.java
+++ Lib/src/main/java/lib/task/IModuleTask.java
@@ -3,7 +3,7 @@ package lib.task;
 /**
  * Created by Justin on 22.11.2014.
  */
-public interface IModuleTask extends Runnable, Comparable {
+public interface IModuleTask extends Runnable, Comparable<IModuleTask> {
     public void setModuleTaskData (String key, Object object);
     public Object getModuleTaskData (String key);
     public void setPriority (int priority);
