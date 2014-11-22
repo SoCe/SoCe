ce76928ee976530030fa7a4d816f8077fb23e01a
JuKu <kuenzel.justin@t-online.de>
2014-11-22 18:59:44 +0100

    implement SoCeQueue.

--- Lib/src/main/java/lib/queue/impl/SoCePriorityQueue.java
+++ Lib/src/main/java/lib/queue/impl/SoCePriorityQueue.java
@@ -2,34 +2,61 @@ package lib.queue.impl;
 
 import lib.queue.ISoCePriorityQueue;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Justin on 22.11.2014.
  */
 public class SoCePriorityQueue<T extends Comparable<T>> implements ISoCePriorityQueue<T> {
+
+    protected List<T> queue = new ArrayList<T>();
+
     @Override
     public void sortQueue() {
-        //
+        Collections.sort(this.queue);
     }
 
     @Override
     public T next() {
-        return null;
+        //sort queue
+        this.sortQueue();
+
+        //First In, first out
+        if (this.queue.size() > 0) {
+            T entry = this.queue.get(0);
+            this.queue.remove(entry);
+
+            return entry;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void addEntry(T object) {
-        //
+        this.queue.add(object);
     }
 
     @Override
     public T getEntry(int i) {
-        return null;
+        if (this.queue.size() >= i) {
+            //get entry from queue
+            T entry = this.queue.get(i);
+
+            //remove item from queue
+            this.queue.remove(entry);
+
+            return entry;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return this.queue.iterator();
     }
 }
--- Lib/src/main/java/lib/queue/impl/SoCeQueue.java
+++ Lib/src/main/java/lib/queue/impl/SoCeQueue.java
@@ -2,29 +2,52 @@ package lib.queue.impl;
 
 import lib.queue.ISoCeQueue;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Justin on 22.11.2014.
  */
 public class SoCeQueue<T> implements ISoCeQueue<T> {
+
+    protected List<T> queue = new ArrayList<T>();
+
     @Override
     public T next() {
-        return null;
+        //First In, first out
+        if (this.queue.size() > 0) {
+            T entry = this.queue.get(0);
+            this.queue.remove(entry);
+
+            return entry;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void addEntry(T object) {
-        //
+        this.queue.add(object);
     }
 
     @Override
     public T getEntry(int i) {
-        return null;
+        if (this.queue.size() >= i) {
+            //get entry from queue
+            T entry = this.queue.get(i);
+
+            //remove item from queue
+            this.queue.remove(entry);
+
+            return entry;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return this.queue.iterator();
     }
 }
