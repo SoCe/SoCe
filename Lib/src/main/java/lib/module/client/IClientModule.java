dfd729e02b02b197c4c273428ec2b085a4d3423e
JuKu <kuenzel.justin@t-online.de>
2014-11-22 18:47:04 +0100

    added interface IClientModule and interface IServerModule.

---
+++ Lib/src/main/java/lib/module/client/IClientModule.java
@@ -0,0 +1,9 @@
+package lib.module.client;
+
+import lib.module.IModule;
+
+/**
+ * Created by Justin on 22.11.2014.
+ */
+public interface IClientModule extends IModule {
+}
---
+++ Lib/src/main/java/lib/module/server/IServerModule.java
@@ -0,0 +1,9 @@
+package lib.module.server;
+
+import lib.module.IModule;
+
+/**
+ * Created by Justin on 22.11.2014.
+ */
+public interface IServerModule extends IModule {
+}
