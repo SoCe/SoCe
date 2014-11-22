29148bf8feff7146fb6728f760ddefb037ddb267
JuKu <kuenzel.justin@t-online.de>
2014-11-22 15:09:52 +0100

    added interface Comparable to ModuleTask.

--- Lib/src/main/java/lib/task/IModuleTask.java
+++ Lib/src/main/java/lib/task/IModuleTask.java
@@ -3,7 +3,7 @@ package lib.task;
 /**
  * Created by Justin on 22.11.2014.
  */
-public interface IModuleTask extends Runnable {
+public interface IModuleTask extends Runnable, Comparable {
     public void setModuleTaskData (String key, Object object);
     public Object getModuleTaskData (String key);
     public void setPriority (int priority);
