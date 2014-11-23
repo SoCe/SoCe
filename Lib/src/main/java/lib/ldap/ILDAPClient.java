7dc311cdaf8848160b2fb577c0433b964ddddbd2
JuKu <kuenzel.justin@t-online.de>
2014-11-23 18:28:47 +0100

    added LDAP integration.

--- Lib/src/main/java/lib/ldap/ILDAPClient.java
+++ Lib/src/main/java/lib/ldap/ILDAPClient.java
@@ -4,4 +4,6 @@ package lib.ldap;
  * Created by Justin on 23.11.2014.
  */
 public interface ILDAPClient {
+    public void connect ();
+    public void close ();
 }
--- Lib/src/main/java/lib/ldap/impl/SoCeLDAPClient.java
+++ Lib/src/main/java/lib/ldap/impl/SoCeLDAPClient.java
@@ -6,4 +6,13 @@ import lib.ldap.ILDAPClient;
  * Created by Justin on 23.11.2014.
  */
 public class SoCeLDAPClient implements ILDAPClient {
+    @Override
+    public void connect() {
+        //http://www.stefan-seelmann.de/media/presentations/JUGM2008_JavaUndLDAP.pdf
+    }
+
+    @Override
+    public void close() {
+        //
+    }
 }
