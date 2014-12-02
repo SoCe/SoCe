5098f727f0f370955d01f026023e92d9676d08be
JuKu <kuenzel.justin@t-online.de>
2014-12-02 20:25:40 +0100

    change pom.xml dependencies.

--- Lib/src/main/java/lib/administration/ISoCeMenuManager.java
+++ Lib/src/main/java/lib/administration/ISoCeMenuManager.java
@@ -1,7 +1,11 @@
 package lib.administration;
 
+import org.eclipse.swt.widgets.Menu;
+
 /**
  * Created by Justin on 02.12.2014.
  */
 public interface ISoCeMenuManager {
+    public void setMenu (String menuName);
+    public void putMenu (String menuName, Menu menu);
 }
--- SoftwareCenter/src/main/java/administration/impl/SoCeMenuManager.java
+++ SoftwareCenter/src/main/java/administration/impl/SoCeMenuManager.java
@@ -1,9 +1,25 @@
 package administration.impl;
 
-import lib.administration.ISoCeMenuManager;
+        import lib.administration.ISoCeMenuManager;
+        import org.eclipse.swt.SWT;
+        import org.eclipse.swt.widgets.Menu;
+        import org.eclipse.swt.widgets.Shell;
+
+        import java.util.HashMap;
 
 /**
  * Created by Justin on 02.12.2014.
  */
 public class SoCeMenuManager implements ISoCeMenuManager {
+
+    protected HashMap<String,Menu> menuHashMap = new HashMap<String,Menu>();
+    protected Shell shell = null;
+
+    public SoCeMenuManager (Shell shell) {
+        this.shell = shell;
+
+        Menu mainMenu = new Menu(shell, SWT.BAR);
+        this.menuHashMap.put("main", mainMenu);
+    }
+
 }
